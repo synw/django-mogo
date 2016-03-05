6f32641f40df470b3525b8cd3d5392699e499cfd
syn <synwe@yahoo.fr>
2016-03-05 16:35:43 +0000

    Corrections activation

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ pip install 'django>=1.8.0,<1.9.0'
 django-admin startproject $1
 cd $1
 
-#~ pip stuff
+#~ install modules
 echo "==================================== Installing python modules =================================="
 pip install ipython pillow django-debug-toolbar django-nose coverage django-braces django-admin-bootstrapped django-bootstrap-form django-bootstrap3 django-mptt django-allauth pytz sorl-thumbnail django-autoslug django-ckeditor django-codemirror2 django-reversion django-filer
 git clone https://github.com/synw/django-jssor.git && mv django-jssor/jssor . && mkdir media && mkdir media/jssor && mkdir media/jssor/thumbnails && rm -rf django-jssor
 git clone https://github.com/synw/django-zongo.git && mv django-zongo/zongo . && mkdir media/zongo && rm -rf django-zongo
 git clone https://github.com/synw/django-alapage.git && mv django-alapage/alapage . && rm -rf django-alapage
 git clone https://github.com/synw/django-dirtyedit.git && cp -r django-dirtyedit/dirtyedit . && rm -rf django-dirtyedit
-	
+
 #~ static stuff
 echo "=============================== Installing external static files ================================"
 mkdir static && cd static && mkdir js && mkdir icons
@@ -31,26 +31,27 @@ cd ../..
 #~ config
 echo "================== Installing config files, templates and internal staticfiles =================="
 git clone https://github.com/synw/django-mogo.git
-rm $1/urls.py && mv django-mogo/urls.py $1
+rm -f $1/urls.py && mv django-mogo/urls.py $1
 mv django-mogo/static/css static
 mv django-mogo/static/js/utils.js static/js
 mv django-mogo/templates .
 
 echo "==================================== Generating settings ==========================================="
-rm $1/settings.py
+rm -f $1/settings.py
 touch $1/settings.py
 mv django-mogo/create_settings.py .
 python create_settings.py $1
 rm -rf django-mogo
-rm create_settings.py
+rm -f create_settings.py
+echo "Settings generated for project $1"
 
 if [ -z "$2" ]
         then
         	echo "==================================== Activating instance ==========================================="
-        	source bin/activate
-			python $1/manage.py makemigrations
-			python $1/manage.py migrate
-			python $1/manage.py createsuperuser
+        	source ./bin/activate
+			python ./$1/manage.py makemigrations
+			python ./$1/manage.py migrate
+			python ./$1/manage.py createsuperuser
 			echo ">>> Installation completed: ready to runserver"
         else
         if [[ "$2" == '-na' ]]
