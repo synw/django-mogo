fe4cba319ed5a9a40abc2c7b54ad22b32b903cc3
syn <synwe@yahoo.fr>
2016-06-10 17:06:25 +0000

    Small corrections

--- djangomogo/create_settings.py
+++ djangomogo/create_settings.py
@@ -112,7 +112,6 @@ INSTALLED_APPS = (
     'sysmon',
     'mbase',
     'mqueue',
-    #'mgof',
     'alapage',
     'dirtyedit',
     'jssor',
@@ -188,17 +187,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-\"""
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = (
                     os.path.join(BASE_DIR, 'static'),
                     )
-\"""
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
