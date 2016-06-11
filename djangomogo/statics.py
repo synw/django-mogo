93b3876ea9c0734fde05d5dd690c89cb09a60cf1
syn <synwe@yahoo.fr>
2016-06-11 17:14:47 +0000

    Corrections

--- djangomogo/statics.py
+++ djangomogo/statics.py
@@ -9,13 +9,13 @@ def modsettings(filepath, mode='normal'):
     s = open(filepath).read()
     if mode == 'collectstatic':
         s = s.replace(
-                      "#STATIC_ROOT = os.path.join(BASE_DIR, 'static')\nSTATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')",
-                      "STATIC_ROOT = os.path.join(BASE_DIR, 'static')\n#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')",
+                      "#STATIC_ROOT = os.path.join(BASE_DIR, 'static')\nSTATICFILES_DIRS = os.path.join(BASE_DIR, 'static')",
+                      "STATIC_ROOT = os.path.join(BASE_DIR, 'static')\n#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')",
                       )
     else:
         s = s.replace(
-                      "STATIC_ROOT = os.path.join(BASE_DIR, 'static')\n#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')",
-                      "#STATIC_ROOT = os.path.join(BASE_DIR, 'static')\nSTATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')",
+                      "STATIC_ROOT = os.path.join(BASE_DIR, 'static')\n#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')",
+                      "#STATIC_ROOT = os.path.join(BASE_DIR, 'static')\nSTATICFILES_DIRS = os.path.join(BASE_DIR, 'static')",
                       )
     f = open(filepath, 'w')
     f.write(s)
