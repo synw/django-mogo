e6d60f255b52df708528dd4107fed0f04d62ffb7
syn <synwe@yahoo.fr>
2016-06-12 15:46:21 +0000

    New Django 1.9 url format

--- djangomogo/urls.py
+++ djangomogo/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.conf.urls import patterns
 from filebrowser.sites import site
 
 from zinnia.sitemaps import TagSitemap
@@ -10,13 +9,8 @@ from zinnia.sitemaps import EntrySitemap
 from zinnia.sitemaps import CategorySitemap
 from zinnia.sitemaps import AuthorSitemap
 
-if settings.DEBUG_TOOLBAR:
-    import debug_toolbar
-    urlpatterns = patterns(
-        '',
-        url(r'^__debug__/', include(debug_toolbar.urls)),)
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('allauth.urls')),
@@ -24,22 +18,29 @@ urlpatterns = patterns('',
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
     url(r'^comments/', include('django_comments.urls')),
-    )
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+
+if settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+"""
 sitemaps = {'tags': TagSitemap,
             'blog': EntrySitemap,
             'authors': AuthorSitemap,
             'categories': CategorySitemap,}
 
-urlpatterns += patterns(
+urlpatterns += [
     'django.contrib.sitemaps.views',
     url(r'^sitemap.xml$', 'index',
         {'sitemaps': sitemaps}),
     url(r'^sitemap-(?P<section>.+)\.xml$', 'sitemap',
-        {'sitemaps': sitemaps}),)
-    
-urlpatterns += url(r'^', include('alapage.urls')),
+        {'sitemaps': sitemaps}),
+    ]
+"""
+urlpatterns += [url(r'^', include('alapage.urls'))]
