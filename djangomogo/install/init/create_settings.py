9ced4eaf8f448e8335156af13e368bd098d90e5b
Win <synwe@yahoo.com>
2017-04-19 11:44:11 +0000

    Moved guardian in installed apps order

--- djangomogo/install/init/create_settings.py
+++ djangomogo/install/init/create_settings.py
@@ -94,7 +94,6 @@ INSTALLED_APPS = (
     'allauth.socialaccount',
     "dirtyedit",
     'reversion',
-    "guardian",
     "compressor",
     'mptt',
     'mbase',
--- djangomogo/install/pages/install.sh
+++ djangomogo/install/pages/install.sh
@@ -22,7 +22,7 @@ mv django-vvpages/vvpages . && rm -rf django-vvpages
 pip install -r $base_dir/djangomogo/install/pages/requirements.txt
 urls="url(r'^pages/',include('vvpages.urls')),#!#url(r'^ckeditor/',include('ckeditor_uploader.urls')),#!#url(r'^graph/',include('mptt_graph.urls')),"
 echo "Updating settings ..."
-python3 $pyscript $project_name $base_dir rest_framework,ckeditor,ckeditor_uploader,codemirror2,mptt_graph,vvpages
+python3 $pyscript $project_name $base_dir rest_framework,ckeditor,ckeditor_uploader,codemirror2,mptt_graph,vvpages,guardian
 echo "Updating urls ..."
 python3 $urlscript $project_name $base_dir $urls vvpages
 check "Pages management app installed"
