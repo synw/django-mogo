bbaaf1d2b0b7ab9a28f50f32cc28faae80076341
joe <synwe@yahoo.com>
2017-08-03 12:47:11 +0000

    Replace fontawesome

---
+++ djangomogo/static/icon/js/icon.js
@@ -0,0 +1,10 @@
+
+function toggleCodes(on) {
+var obj = document.getElementById('icons');
+
+if (on) {
+  obj.className += ' codesOn';
+} else {
+  obj.className = obj.className.replace(' codesOn', '');
+}
+}
